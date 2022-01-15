75aa241a49a8b98f75e790ebea80442fb38d7195
Fhwang <hdh0926@naver.com>
2022-01-15 21:22:42 +0900

    fix: build test

--- mySns/mySns/src/test/java/com/spring/mysns/mySns/MySnsApplicationTests.java
+++ mySns/mySns/src/test/java/com/spring/mysns/mySns/MySnsApplicationTests.java
@@ -1,9 +1,11 @@
 package com.spring.mysns.mySns;
 
+import com.app.mysns.MySnsApplication;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MySnsApplication.class)
 class MySnsApplicationTests {
 
 	@Test
