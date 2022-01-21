81a4a5e3672c5217edc3cf2c86be1f5e7f8c4bad
JEONINSUCK <wdt0818@naver.com>
2022-01-21 13:21:21 +0900

    (facebook.js): change the print format

--- scraping/facebook.js
+++ scraping/facebook.js
@@ -1,4 +1,6 @@
 const peppeteer = require("puppeteer");
+const dotenv = require('dotenv');
+dotenv.config();
 const screenShot = 'facebook.png'
 const scraper = async() => {
     try {
@@ -9,40 +11,58 @@ const scraper = async() => {
         await page.setViewport({
             width:1920,
             height:1080
-        })
-        const id = "wdt0818@naver.com";
-        const pw = "jeon5376!!";
+        });
+
         await page.goto('https://facebook.com');
         
         await page.evaluate((id, pw) => {
             document.querySelector("#email").value = id;
             document.querySelector("#pass").value = pw;
-//            document.querySelector("button[type=submit]").click();
-        }, id, pw)
+        }, process.env.EMAIL, process.env.PASSWORD)
 
         await page.click("button[type=submit]");
         await page.waitForNavigation();
-        // await page.keyboard.press("Escape");
-        // await page.screenshot({path: screenShot});
 
         await page.goto('https://www.facebook.com/settings?tab=applications&ref=settings');
+        await page.waitFor(500);
 
-        const loginList = await page.$x('.//div[@class="pow20xho"]/div/div/div/div/div/div/span[@class="d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d3f4x2em iv3no6db jq4qci2q a3bd9o3v lrazzd5p oo9gr5id hzawbc8m"]');
-        const text = await page.evaluate(loginList => loginList.textContent, loginList[1]);
-        console.log(text);
+        const xpath = './/div[@class="' + process.env.MAIN_CLASS + '"]/div/div/div/div/div/div/span[@class="' + process.env.SPAN_CLASS + '"]';
+        
+        // const loginList = await page.$x(process.env.XPATH);
+        const loginList = await page.$x(xpath);
+        // const loginList = await page.$('.//pow20xho');
 
+        let data = {};
+        let datas = [];
+
+        if(loginList.length == 0)
+        {
+            console.log("Can't find data");
+        }
+        else{
+            for(let i=0; i<loginList.length; i++)
+            {   
+                data.name =  await page.evaluate((data) => {
+                    return data.textContent;
+                }, loginList[i]);
+
+                datas.push({
+                    title : data.name,
+                    type : 'facebook',
+                    url : ' ',
+                    join_at : ' '
+                });
+            }
+            console.log(datas);
+        }
         browser.close();
     } catch(err) {
         console.log(err);
     }
+
+    finally{
+    }
 }
 
-// const screenShot = 'facebook.png';
-
-// const scraper2 = async() => {
-//     try {
-//         const
-//     }
-// }
 
 scraper();
