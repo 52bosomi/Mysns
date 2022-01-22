47e38d05a094a0685ac50c5b567bd5d046ff9e6c
ANCJaneKim <ANC.JaneKim@gmail.com>
2022-01-22 17:25:12 +0900

    DB DTO With lombok

--- mySns/mySns/src/main/java/com/app/mysns/dto/ClientDto.java
+++ mySns/mySns/src/main/java/com/app/mysns/dto/ClientDto.java
@@ -5,10 +5,20 @@ import java.sql.Timestamp;
 import lombok.Data;
 @Data
 public class ClientDto {
+    private int id;
     private String username;
     private String password;
     private String name;
-    private String phone;
+    //private String phone;
+    private Boolean use_facebook;  
+    private String access_facebook;
+    private String refresh_facebook; 
+    private Boolean use_instagram;  
+    private String access_instagram;
+    private Boolean use_naver;  
+    private String access_naver;
+    private String refresh_naver;
     private Timestamp updated_at;
     private Timestamp created_at;
 }
+
