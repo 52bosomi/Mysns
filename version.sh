1bad9867ca6b703d08e76cad1eda2b200678adb5
Fhwang <hdh0926@naver.com>
2022-01-31 16:56:09 +0900

    test

--- version.sh
+++ version.sh
@@ -4,5 +4,5 @@ echo $RELEASE_VERSION
 pwd
 ls -al
 sed -i 's/version = /\/\/version/' ./mySns/mySns/build.gradle
-echo version=$RELEASE_VERSION >> ./mySns/mySns/build.gradle
+echo version='$RELEASE_VERSION' >> ./mySns/mySns/build.gradle
 cat ./mySns/mySns/build.gradle
