fe045e3c15b2b6d0c5e7b253d8401a9dfdbea12d
Fhwang <hdh0926@naver.com>
2022-01-31 15:50:05 +0900

    test

--- version.sh
+++ version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo $GITHUB_ENV
 echo $RELEASE_VERSION
+pwd
 sed -i 's/version/\/\/version/' mysns/mysns/build.gradle
 echo version=$RELEASE_VERSION >> mysns/mysns/build.gradle
 cat mysns/mysns/build.gradle
