b5e33875fcd963a9fa1457f675512c0caf7d7208
Fhwang <hdh0926@naver.com>
2022-02-06 01:06:16 +0900

    add_mapper

--- mySns/mySns/src/main/java/com/app/mysns/service/SecureUtilsService.java
+++ mySns/mySns/src/main/java/com/app/mysns/service/SecureUtilsService.java
@@ -3,32 +3,6 @@ package com.app.mysns.service;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
-// public class UserSha256 {
-//     public static String encrypt(String planText) {
-//         try {
-//             MessageDigest md = MessageDigest.getInstance("SHA-256");
-//             md.update(planText.getBytes());
-//             byte byteData[] = md.digest();
-//             StringBuffer sb = new StringBuffer();
-//             for (int i = 0; i < byteData.length; i++) {
-//                 sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-//             }
-//             StringBuffer hexString = new StringBuffer();
-//             for (int i = 0; i < byteData.length; i++) {
-//                 String hex = Integer.toHexString(0xff & byteData[i]);
-//                 if (hex.length() == 1) {
-//                     hexString.append('0');
-//                 }
-//                 hexString.append(hex);
-//             }
-//             return hexString.toString();
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//             throw new RuntimeException();
-//         }
-//     }
-
-// }
 
 // @유가희 참고!!
 // https://www.javaguides.net/2020/02/java-sha-256-hash-with-salt-example.html
