4051167eabdbf98c1529c4a96fcfd510f9c34c39
Fhwang <hdh0926@naver.com>
2022-02-08 00:35:36 +0900

    t

--- mySns/mySns/src/main/java/com/app/mysns/MySnsApplication.java
+++ mySns/mySns/src/main/java/com/app/mysns/MySnsApplication.java
@@ -2,21 +2,12 @@ package com.app.mysns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.web.servlet.ServletComponentScan;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
-// @ServletComponentScan(basePackages = "com.app.mysns.filter")
 @EnableCaching
 public class MySnsApplication {
-
-    // @Bean
-    // public ServerEndpointExporter serverEndpointExporter() {
-    //     return new ServerEndpointExporter();
-    // }
 
     public static void main(String[] args) {
         SpringApplication.run(MySnsApplication.class, args);
