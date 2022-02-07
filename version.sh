ecabdf07226826d14f70edef6e5fbbe91af6fb05
Fhwang <hdh0926@naver.com>
2022-02-07 19:52:04 +0900

    teest

--- version.sh
+++ version.sh
@@ -8,5 +8,5 @@ echo version=\'`echo $RELEASE_VERSION`\' >> ./mySns/mySns/build.gradle
 cat ./mySns/mySns/build.gradle
 
 sed -i 's/spring.redis.host = /#application.properties/' ./mySns/mySns/src/main/resources/application.properties
-echo version=\'`echo spring.redis.host=172.17.0.1`\' >> ./mySns/mySns/src/main/resources/application.properties
+echo spring.redis.host=172.17.0.1 >> ./mySns/mySns/src/main/resources/application.properties
 cat ./mySns/mySns/src/main/resources/application.properties
