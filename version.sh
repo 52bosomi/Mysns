987a4298928bca69957ba2f10ec0922a16b7d758
Fhwang <hdh0926@naver.com>
2022-02-07 19:58:35 +0900

    teste

--- version.sh
+++ version.sh
@@ -7,6 +7,6 @@ sed -i 's/version = /\/\/version/' ./mySns/mySns/build.gradle
 echo version=\'`echo $RELEASE_VERSION`\' >> ./mySns/mySns/build.gradle
 cat ./mySns/mySns/build.gradle
 
-sed -i 's/spring.redis.host = /#application.properties/' ./mySns/mySns/src/main/resources/application.properties
+sed -i 's/spring.redis.host = /#spring.redis.host/' ./mySns/mySns/src/main/resources/application.properties
 echo spring.redis.host=172.17.0.1 >> ./mySns/mySns/src/main/resources/application.properties
 cat ./mySns/mySns/src/main/resources/application.properties
