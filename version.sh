14b10b2c0023d7720b81595c55a8d5f65a1571b2
hdh <hdh0926@naver.com>
2022-02-07 11:14:16 +0900

    fix: add redis bug fix

--- version.sh
+++ version.sh
@@ -6,3 +6,7 @@ ls -al
 sed -i 's/version = /\/\/version/' ./mySns/mySns/build.gradle
 echo version=\'`echo $RELEASE_VERSION`\' >> ./mySns/mySns/build.gradle
 cat ./mySns/mySns/build.gradle
+
+sed -i 's/spring.redis.host = /#application.properties/' ./mySns/mySns/src/main/resources/application.properties
+echo version=\'`echo spring.redis.host=172.17.0.1`\' >> ./mySns/mySns/src/main/resources/application.properties
+cat ./mySns/mySns/src/main/resources/application.properties
