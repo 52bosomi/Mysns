825d1ebf1ff58cf79e592d724abfcc74e25d5929
JEONINSCUK <wdt0818@naver.com>
2022-02-13 15:02:26 +0900

    Server WebSocket update

--- scraping/cli.js
+++ scraping/cli.js
@@ -1,22 +1,65 @@
 const websock = require("socket.io-client")
+const 
 
-const socket_cli = websock("ws://localhost:8888")
+const sample_data = {   username: "wdt0818@naver.com",
+                        password: "jeon5376!!",
+                        type: "facebook",
+                        uuid: "",
+                        ua: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+                    };
 
-socket_cli.on("connect", (msg) => {
-    console.log("connection")
-    socket_cli.emit('loginInfo', {email: "wdt0818@naver.com", password: "jeon5376!!", type: "facebook"})
-    socket_cli.on("disconnecting", () => {
-        console.log(socket_cli.id);
-    })
-    // socket_cli.disconnect();
-    // if(socket_cli.disconnected)
-    // {
-    //     console.log("disconnected")
-        
-    // } 
-})
+async function DockerComm()
+{
+    try{
+        const socketDocker = await websock("ws://localhost:8888", {
+            transports:  ["polling", "websocket"]
+            })
+
+        await socketDocker.on("connect", () => {
+            const engine = socketDocker.io.engine;
+
+            /* login info send */
+            socketDocker.emit('loginInfo', sample_data);
+
+            /* response proccess */
+            socketDocker.on("response", async (req) =>{
+                console.log(req);
+                // send thd result to web
+            })
+
+            // engine.on("close", (reason) => {
+            //     console.log("socketDocker closed\n(" + reason + ")");
+            // })
+
+            /* docker socket disconnection msg */
+            socketDocker.on("disconnect", (reason) =>{
+                if(reason == "io server disconnect")
+                    console.log("socketDocker disconnected\n(" + reason + ")");    
+                else if(reason == "io client disconnect")
+                    console.log("socketDocker disconnected\n(" + reason + ")");
+                else if(reason == "ping timeout")
+                    console.log("socketDocker disconnected\n(" + reason + ")");
+                else if(reason == "transport close")
+                    console.log("socketDocker disconnected\n(" + reason + ")");
+                else if(reason == "transport error")
+                    console.log("socketDocker disconnected\n(" + reason + ")");
+            })
+        });
+
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
 
 
-socket_cli.on("response", req => {
-    console.log(req)
-})
+
+try
+{
+    DockerComm();
+}
+catch(err)
+{
+    console.log(err);
+}
