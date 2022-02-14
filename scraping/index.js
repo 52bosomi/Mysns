138656bda5b95b5258f71f8933160067f9cbe683
JEONINSUCK <wdt0818@naver.com>
2022-02-15 00:04:33 +0900

    socket send add

--- scraping/index.js
+++ scraping/index.js
@@ -124,7 +124,18 @@ async function Run()
                 // if(Object.keys(webMsg.data).length = Object.keys(webData).length)
                 // {
                 //     webData = Object.assign(webMsg.data);
-                //     scrapingData = await DockerComm();
+
+                //     /* Setting the snsFlag object */
+                //     for (var i=0; i<Object.keys(snsFlag).length; i++)
+                //     {   
+                //         if(webData.type == Object.keys(snsFlag)[i])
+                //         {
+                //             snsFlag[Object.keys(snsFlag)[i]] = 1;
+                //         }
+                //     }
+                    
+                //     let scrapingData = await MainLoop(webData);
+                //     // console.log(scrapingData);
                 //     await ResponseProcess(scrapingData);
                 //     console.log(webData);
                 // }
@@ -133,7 +144,8 @@ async function Run()
                 //     await ErrorProcesss("data length not match");
                 //     console.log(webData);
                 // }
-    
+                
+                /* temp code */
                 if((Object.keys(sample_data).length) == (Object.keys(webData).length))
                 {   
                     webData = Object.assign(sample_data);
@@ -151,6 +163,9 @@ async function Run()
                     // console.log(scrapingData);
                     await ResponseProcess(scrapingData);
                     console.log(webData);
+                    
+                    /* socket send */
+                    await socketWeb.send(webData);
                 }
                 else
                 {
